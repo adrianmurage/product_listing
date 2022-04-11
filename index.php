5efc13b41ecdeb42ab0a67d60434dbf33c65fe16
Adrian Murage <adrianmurage21@gmail.com>
2022-04-11 14:51:09 +0300

    try and see if the connection works on the cloud

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
 <?php 
-echo "Hello there Heroku, Adrian here!"
+
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+if (!$conn){
+    echo "Connection falied!";
+} else {
+    echo 'Connection successful!' .'<br>';
+};
+
+
+$query = "SELECT * FROM test_table";
+$result = $conn->query($query);
+
+$rows = $result->num_rows;
+
+for ($i = 0; $i < $rows; ++$i){
+    $result->data_seek($i);
+    echo 'First Name: ' .htmlspecialchars($result->fetch_assoc()['FirstName']) .'<br>';
+}
 ?>
