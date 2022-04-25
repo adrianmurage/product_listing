b926a4c1e69045bd29108918529dd89d8a04f840
Adrian Murage <adrianmurage21@gmail.com>
2022-04-25 10:00:31 +0300

    attempt heroku autoloader fix

--- includes/classautoloader.inc.php
+++ includes/classautoloader.inc.php
@@ -8,7 +8,7 @@ function autoLoader($className)
     $url = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 
     if (strpos($url, 'includes') !== false) {
-        $path = '../classes/';
+        $path = '/../../classes/';
     } else {
 
         $path = "classes/";
