2c20ac4113dd5eaf370931526a2919b94fe303c7
Adrian Murage <adrianmurage21@gmail.com>
2022-05-26 23:18:03 +0300

    Experiment with receiving POST requests

--- submitform.php
+++ submitform.php
@@ -3,20 +3,29 @@
 include_once './classes/test.class.php';
 include_once './classes/dbh.class.php';
 
-$first_name = array_key_exists('first_name', $_POST) ? $_POST['first_name'] : "";
-$last_name = array_key_exists('last_name', $_POST) ? $_POST['last_name'] : "";
+$sku = array_key_exists('sku', $_POST) ? $_POST['sku'] : "";
+
+echo $sku; 
+
+// <?php
+
+// include_once './classes/test.class.php';
+// include_once './classes/dbh.class.php';
+
+// $first_name = array_key_exists('first_name', $_POST) ? $_POST['first_name'] : "";
+// $last_name = array_key_exists('last_name', $_POST) ? $_POST['last_name'] : "";
 
 
-echo $first_name; 
-echo $last_name; 
+// echo $first_name; 
+// echo $last_name; 
 
-$insertUser = new Test();
-$insertUser->setNewUser($first_name, $last_name);
+// $insertUser = new Test();
+// $insertUser->setNewUser($first_name, $last_name);
 
-$environment = getenv("ENVIRONMENT");
+// $environment = getenv("ENVIRONMENT");
 
-if ($environment == "development"){
-    header('Location:/productlist/');
-} elseif ($environment == "production") {
-    header('Location:/');
-}
+// if ($environment == "development"){
+//     header('Location:/productlist/');
+// } elseif ($environment == "production") {
+//     header('Location:/');
+// }
