6a8b13dd9604491e136a0ed5ded9ea07461b76b4
Adrian Murage <adrianmurage21@gmail.com>
2022-05-28 10:44:25 +0300

    Style the product list page

--- productlist.php
+++ productlist.php
@@ -7,19 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
     <script src="./load_data.js"></script>
+    <link rel="stylesheet" href="./productlist.css">
     <title>Product List</title>
 </head>
 
 <body>
-    <h1>Product List</h1>
-    <form method="GET" action="./addproduct.php">
-        <input type="submit" value="ADD">
-    </form>
-    <button id="mass-delete">MASS DELETE</button>
+    <section class="header">
+        <h1 class="page_heading">Product List</h1>
+        <button class="massdelete_btn" id="mass-delete">MASS DELETE</button>
+        <input class="addproduct_btn" form="add_product"  type="submit" value="ADD">
+    </section>
+
     <hr>
     <div id="productlist">
 
     </div>
+    <form id="add_product"method="GET" action="./addproduct.php">
+    </form>
 </body>
 
 </html>
